76570eabebfa2dccbc47ff2a1622c49ab307e031
kienntk2810 <102545669+kienntp45@users.noreply.github.com>
2022-07-11 14:59:35 +0700

    demo microservice

---
+++ Iot.Class.Data/Migrations/20220421181253_kieu.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Iot.Class.Data.Migrations
+{
+    public partial class kieu : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Classes",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    ClassName = table.Column<string>(type: "text", nullable: true),
+                    Teacher = table.Column<string>(type: "text", nullable: true),
+                    PhoneNumber = table.Column<string>(type: "text", nullable: true),
+                    Room = table.Column<int>(type: "integer", nullable: false),
+                    IsDeleted = table.Column<bool>(type: "boolean", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Classes", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Classes");
+        }
+    }
+}
---
+++ Iot.Class.Infatructure/Queries/GetClassAllQuery.cs
@@ -0,0 +1,13 @@
+﻿using Iot.Class.Domain.Dtos;
+using Iot.Class.Domain.ReadModels;
+using MediatR;
+
+namespace Iot.Class.Infatructure.Queries;
+
+public record GetClassAllQuery : IRequest<IQueryable<ClassDto>>
+{
+    public GetClassAllQuery()
+    {
+        
+    }
+}
